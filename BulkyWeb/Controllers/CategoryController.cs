d850170e6fdec30db459040886e485b5cb295f77
ksrisahithi <srisahithi333@gmail.com>
2024-05-27 19:51:53 +0530

    category CRUD operations

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,5 +16,75 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
-    }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+		public IActionResult Create(Category obj)
+		{
+           if(ModelState.IsValid)
+            {
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category Created Successfully";
+				return RedirectToAction("Index");
+			}
+           return View();
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if(id == null || id == 0 )
+			{
+				return NotFound();
+			}
+			Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+			if(category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category Updated Successfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if(obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category Deleted Successfully";
+			return RedirectToAction("Index");
+		}
+	}
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -7,7 +8,12 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
+        
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Display Order must be in the range 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
